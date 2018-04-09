0aebe312d1ddee3d3ec0b94b0e526a94a5d602a8
WangLuming <599290100@qq.com>
2018-04-09 19:43:42 +0900

    day22 #206

---
+++ src/ReverseList.java
@@ -0,0 +1,35 @@
+
+/*LeetCode
+day22 #206.
+ */
+Reverse Linked List
+        */
+        DescriptionHintsSubmissionsDiscussSolution
+
+        Reverse a singly linked list.*/
+
+
+//  Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode root = new ListNode(0);
+        ListNode nextNode;
+        while (head != null) {
+            nextNode = head.next;
+            head.next = root.next;
+            root.next = head;
+            head = nextNode;
+        }
+        return root.next;
+    }
+}
+}

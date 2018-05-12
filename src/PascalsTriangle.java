2e440acfc3cb8931895359606fc95af46232a6a3
WangLuming <599290100@qq.com>
2018-05-13 02:49:16 +0900

    day44 #118

---
+++ src/PascalsTriangle.java
@@ -0,0 +1,39 @@
+/*LeetCode
+day44 #118. Pascal's Triangle
+        DescriptionHintsSubmissionsDiscussSolution
+        Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+        In Pascal's triangle, each number is the sum of the two numbers directly above it.
+        Example:
+        Input: 5
+        Output:
+        [
+        [1],
+        [1,1],
+        [1,2,1],
+        [1,3,3,1],
+        [1,4,6,4,1]*/
+
+import java.util.List;
+
+public class PascalsTriangle {
+    public class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+            if (numRows <= 0) {
+                return triangle;
+            }
+            for (int i = 0; i < numRows; i++) {
+                List<Integer> row = new ArrayList<Integer>();
+                for (int j = 0; j < i + 1; j++) {
+                    if (j == 0 || j == i) {
+                        row.add(1);
+                    } else {
+                        row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                    }
+                }
+                triangle.add(row);
+            }
+            return triangle;
+        }
+    }
+}
